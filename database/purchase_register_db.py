4b931e160a7e5d3717d81a804092841f0dfadeed
AmanKrSarkar <amanglive@gmail.com>
2026-07-11 10:48:11 +0530

    modified:   database/__pycache__/backup_db.cpython-313.pyc 	modified:   database/__pycache__/company_db.cpython-313.pyc 	modified:   database/__pycache__/inventory_db.cpython-313.pyc 	modified:   database/__pycache__/ledger_db.cpython-313.pyc 	modified:   database/__pycache__/party_db.cpython-313.pyc 	modified:   database/__pycache__/party_master_db.cpython-313.pyc 	modified:   database/__pycache__/payment_db.cpython-313.pyc 	modified:   database/__pycache__/payment_register_db.cpython-313.pyc 	modified:   database/__pycache__/product_db.cpython-313.pyc 	modified:   database/__pycache__/product_master_db.cpython-313.pyc 	modified:   database/__pycache__/purchase_db.cpython-313.pyc 	modified:   database/__pycache__/purchase_register_db.cpython-313.pyc 	modified:   database/__pycache__/purchase_return_db.cpython-313.pyc 	modified:   database/__pycache__/restore_db.cpython-313.pyc 	modified:   database/__pycache__/sales_db.cpython-313.pyc 	modified:   database/__pycache__/sales_register_db.cpython-313.pyc 	modified:   database/__pycache__/sales_return_db.cpython-313.pyc 	modified:   database/__pycache__/stock_adjustment_db.cpython-313.pyc 	modified:   database/__pycache__/stock_ledger_db.cpython-313.pyc 	modified:   database/inventory_db.py 	modified:   database/ledger_db.py 	modified:   database/party_db.py 	modified:   database/party_master_db.py 	modified:   database/payment_db.py 	modified:   database/payment_register_db.py 	modified:   database/product_db.py 	modified:   database/product_master_db.py 	modified:   database/purchase_db.py 	modified:   database/purchase_register_db.py 	modified:   database/purchase_return_db.py 	modified:   database/sales_db.py 	modified:   database/sales_register_db.py 	modified:   database/sales_return_db.py	modified:   database/stock_adjustment_db.py 	modified:   database/stock_ledger_db.py

--- database/purchase_register_db.py
+++ database/purchase_register_db.py
@@ -1,11 +1,4 @@
-import sqlite3
-
 from database.database import get_connection
-
-def get_connection():
-    conn = sqlite3.connect(DB_PATH)
-    conn.row_factory = sqlite3.Row
-    return conn
 
 def get_purchase_register():
 
